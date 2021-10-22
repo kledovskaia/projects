8a96b2cdb32cf8ef81d92f3b1a08cd9001326d65
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-10-22 12:42:14 +0300

    note model updated

--- notedly/api/src/models/note.js
+++ notedly/api/src/models/note.js
@@ -9,6 +9,17 @@ const noteSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
       required: true
+    },
+    favoriteCount: {
+      type: Number,
+      required: true,
+      default: 0
+    },
+    favoritedBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      default: []
     }
   },
   {
