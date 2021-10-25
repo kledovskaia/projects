c503d5def9def768e1f98b0836091f37f98f96f3
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-10-25 05:30:58 +0300

    handleFetchMoreNotes

--- notedly/client/src/pages/Home/Home.tsx
+++ notedly/client/src/pages/Home/Home.tsx
@@ -4,24 +4,53 @@ import { useRequest } from "../../hooks/useRequest"
 import { Container } from "./styles"
 import { NoteFeed } from "../../components/NoteFeed/NoteFeed"
 
+type TResponse = {
+  noteFeed: {
+    notes: TNote[]
+    cursor: TNote["id"]
+    hasNextPage: boolean
+  }
+}
+
 export const Home = () => {
-  const { data, loading, error } = useRequest<{
-    noteFeed: {
-      notes: TNote[]
-    }
-  }>("GET_NOTES")
+  const { data, loading, error, fetchMore } = useRequest<TResponse>("GET_NOTES")
   useDocumentTitle("Home")
 
   useEffect(() => {
     console.log(data)
   }, [data])
 
+  const handleFetchMoreNotes = () => {
+    fetchMore({
+      variables: {
+        cursor: data?.noteFeed.cursor,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) =>
+        ({
+          noteFeed: {
+            ...fetchMoreResult?.noteFeed,
+            notes: [
+              ...previousResult.noteFeed.notes,
+              ...(fetchMoreResult?.noteFeed.notes || []),
+            ],
+          },
+        } as TResponse),
+    })
+  }
+
   if (loading) return <p>Loading...</p>
   if (error) return <p>Error!</p>
 
   return (
     <Container>
-      {data && data.noteFeed && <NoteFeed notes={data.noteFeed.notes} />}
+      {data && data.noteFeed && (
+        <>
+          <NoteFeed notes={data.noteFeed.notes} />
+          {data.noteFeed.hasNextPage && (
+            <button onClick={handleFetchMoreNotes}>Load More</button>
+          )}
+        </>
+      )}
     </Container>
   )
 }
