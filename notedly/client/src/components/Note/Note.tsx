fe5a80978e32ebea4bb9ba15a1dbb835ebedcb31
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-10-28 06:25:53 +0300

    delete & toggleFavorite

--- notedly/client/src/components/Note/Note.tsx
+++ notedly/client/src/components/Note/Note.tsx
@@ -1,7 +1,13 @@
 import { FC, useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { AuthContext } from '../../context/Auth'
+import {
+  GET_FAVORITE_NOTES,
+  GET_MY_NOTES,
+  GET_NOTES,
+} from '../../graphql/queries'
 import { stopBubbling } from '../../helpers/functions'
+import { useAppMutation } from '../../hooks/useAppMutation'
 import {
   AuthorContainer,
   AuthorName,
@@ -22,6 +28,22 @@ type Props = {
 
 export const Note: FC<Props> = ({ note }) => {
   const { data, isLoggedIn } = useContext(AuthContext)
+  const [toggleFavorite, { loading: loadingToggle, error: errorToggle }] =
+    useAppMutation('TOGGLE_FAVORITE', {
+      refetchQueries: [
+        { query: GET_NOTES },
+        { query: GET_MY_NOTES },
+        { query: GET_FAVORITE_NOTES },
+      ],
+    })
+  const [deleteNote, { loading: loadingDelete, error: errorDelete }] =
+    useAppMutation('DELETE_NOTE', {
+      refetchQueries: [
+        { query: GET_NOTES },
+        { query: GET_MY_NOTES },
+        { query: GET_FAVORITE_NOTES },
+      ],
+    })
 
   return (
     <Container>
@@ -31,21 +53,47 @@ export const Note: FC<Props> = ({ note }) => {
           <AuthorName>{note.author.username}</AuthorName>
         </AuthorContainer>
         {data && note.author.id === data.id && (
-          <Link to={`/edit/${note.id}`}>Edit</Link>
+          <>
+            <Link to={`/edit/${note.id}`}>Edit</Link>
+            <button
+              onClick={(e) => {
+                e.stopPropagation()
+                deleteNote({
+                  variables: {
+                    id: note.id,
+                  },
+                })
+              }}
+            >
+              Delete
+            </button>
+          </>
         )}
         {isLoggedIn && (
-          <ToggleFavorite>
+          <ToggleFavorite
+            onClick={(e) => {
+              e.stopPropagation()
+              toggleFavorite({
+                variables: {
+                  id: note.id,
+                },
+              })
+            }}
+          >
             Favorite
             <ToggleFavoriteIcon />
           </ToggleFavorite>
         )}
       </Header>
-      <Content>{note.content}</Content>
+      <Link to={`/note/${note.id}`}>
+        <Content>{note.content}</Content>
+      </Link>
       <Footer>
         <CreatedAt>{note.createdAt}</CreatedAt>
         {note.createdAt !== note.updatedAt && (
           <UpdatedAt>{note.updatedAt}</UpdatedAt>
         )}
+        <div>{note.favoriteCount}</div>
       </Footer>
     </Container>
   )
