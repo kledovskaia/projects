01004f2379aa6a3363604a5ee49b6695a32252be
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-02 08:09:37 +0300

    App layout styles changes

--- kanban-board/src/components/App/styles.ts
+++ kanban-board/src/components/App/styles.ts
@@ -2,9 +2,15 @@ import styled from 'styled-components'
 import macro from 'styled-components/macro'
 
 export const Container = styled.section`
-  display: grid;
-  grid-template-columns: repeat(auto-fit, minmax(25rem, 35rem));
-  grid-template-rows: 1fr;
-  grid-gap: 2.5rem;
+  display: flex;
   padding: 2.5rem;
+
+  & > * {
+    flex-basis: 30rem;
+    flex-shrink: 0;
+  }
+
+  & > *:not(:last-child) {
+    margin-right: 2.5rem;
+  }
 `
