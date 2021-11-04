d5545a6b93d32fdacd27d991d3507c216099dde1
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-04 12:41:41 +0300

    moving between 2 columns

--- kanban-board/src/components/App/App.tsx
+++ kanban-board/src/components/App/App.tsx
@@ -46,10 +46,38 @@ const move = <
 };
 
 export const App = () => {
-  const [state, setState] = useState([getItems(10), getItems(5, 10)]);
+  const [state, setState] = useState([
+    {
+      id: "1",
+      tasks: [
+        { id: "12", content: "Some Content 1" },
+        { id: "22", content: "Some Content 2" },
+        { id: "32", content: "Some Content 3" },
+        { id: "42", content: "Some Content 4" },
+      ],
+    },
+    {
+      id: "2",
+      tasks: [
+        { id: "52", content: "Some Content 5" },
+        { id: "62", content: "Some Content 6" },
+        { id: "72", content: "Some Content 7" },
+        { id: "82", content: "Some Content 8" },
+      ],
+    },
+    {
+      id: "3",
+      tasks: [
+        { id: "92", content: "Some Content 9" },
+        { id: "102", content: "Some Content 10" },
+        { id: "112", content: "Some Content 11" },
+        { id: "122", content: "Some Content 12" },
+      ],
+    },
+  ]);
 
   type OnDragEnd = (result: DropResult, provided: ResponderProvided) => void;
-  const onDragEnd: OnDragEnd = (result) => {
+  const onDragEndTasks: OnDragEnd = (result) => {
     const { source, destination } = result;
 
     // dropped outside the list
@@ -60,55 +88,85 @@ export const App = () => {
     const dInd = +destination.droppableId;
 
     if (sInd === dInd) {
-      const items = reorder(state[sInd], source.index, destination.index);
+      const items = reorder(state[sInd].tasks, source.index, destination.index);
       const newState = [...state];
-      newState[sInd] = items;
+      newState[sInd].tasks = items;
       setState(newState);
     } else {
-      const result = move(state[sInd], state[dInd], source, destination);
+      const result = move(
+        state[sInd].tasks,
+        state[dInd].tasks,
+        source,
+        destination
+      );
       const newState = [...state];
-      newState[sInd] = result[sInd];
-      newState[dInd] = result[dInd];
+      newState[sInd].tasks = result[sInd];
+      newState[dInd].tasks = result[dInd];
 
-      setState(newState.filter((group) => group.length));
+      setState(newState);
     }
+  };
+
+  const onDragEndLists: OnDragEnd = (result) => {
+    const { source, destination } = result;
+    if (!destination) {
+      return;
+    }
+    if (source.index === destination.index) return;
   };
 
   return (
     <div>
-      <Container>
-        <DragDropContext onDragEnd={onDragEnd}>
-          {state.map((el, ind) => (
-            <Droppable key={ind} droppableId={`${ind}`}>
-              {(provided, snapshot) => (
-                <Column>
-                  <Title>Title</Title>
-                  <Card ref={provided.innerRef} {...provided.droppableProps}>
-                    {el.map((item, index) => (
-                      <Draggable
-                        key={item.id}
-                        draggableId={item.id}
-                        index={index}
+      <DragDropContext onDragEnd={onDragEndLists}>
+        <Droppable droppableId="lists">
+          {(provided) => (
+            <Container ref={provided.innerRef} {...provided.droppableProps}>
+              <DragDropContext onDragEnd={onDragEndTasks}>
+                {state.map((list, ind) => (
+                  <Draggable key={list.id} draggableId={list.id} index={ind}>
+                    {(provided) => (
+                      <Column
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
                       >
-                        {(provided, snapshot) => (
-                          <CardItem
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            {...provided.dragHandleProps}
-                          >
-                            {item.content}
-                          </CardItem>
-                        )}
-                      </Draggable>
-                    ))}
-                    {provided.placeholder}
-                  </Card>
-                </Column>
-              )}
-            </Droppable>
-          ))}
-        </DragDropContext>
-      </Container>
+                        <Title>Title</Title>
+                        <Droppable droppableId={`${ind}`}>
+                          {(provided) => (
+                            <Card
+                              ref={provided.innerRef}
+                              {...provided.droppableProps}
+                            >
+                              {list.tasks.map((task, index) => (
+                                <Draggable
+                                  key={task.id}
+                                  draggableId={task.id}
+                                  index={index}
+                                >
+                                  {(provided) => (
+                                    <CardItem
+                                      ref={provided.innerRef}
+                                      {...provided.draggableProps}
+                                      {...provided.dragHandleProps}
+                                    >
+                                      {task.content}
+                                    </CardItem>
+                                  )}
+                                </Draggable>
+                              ))}
+                              {provided.placeholder}
+                            </Card>
+                          )}
+                        </Droppable>
+                      </Column>
+                    )}
+                  </Draggable>
+                ))}
+              </DragDropContext>
+            </Container>
+          )}
+        </Droppable>
+      </DragDropContext>
     </div>
   );
 };
