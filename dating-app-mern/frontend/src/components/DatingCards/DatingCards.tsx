f5926cc86591e5b4db4d1a12d557a2fd801d9df2
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-08 06:36:20 +0300

    swipe buttons

--- dating-app-mern/frontend/src/components/DatingCards/DatingCards.tsx
+++ dating-app-mern/frontend/src/components/DatingCards/DatingCards.tsx
@@ -1,59 +1,136 @@
-import { useState } from "react";
-import DatingCard from "react-tinder-card";
+import React, { useState, useMemo, useRef, RefObject } from "react";
+import TinderCard from "react-tinder-card";
 import { DatingCardsContainer } from "./styles";
 
-type Person = {
-  name: string;
-  imgUrl: string;
+const db = [
+  {
+    name: "Random Guy",
+    url: "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80",
+  },
+  {
+    name: "Another Guy",
+    url: "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    name: "Random Girl",
+    url: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
+  },
+  {
+    name: "Another Girl",
+    url: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
+  },
+];
+
+type Direction = "left" | "right" | "up" | "down";
+
+type ChildRef = {
+  swipe(dir?: Direction): Promise<void>;
+  restoreCard(): Promise<void>;
 };
 
 export const DatingCards = () => {
-  const [people, setPeople] = useState<Person[]>([
-    {
-      name: "Random Guy",
-      imgUrl:
-        "https://images.unsplash.com/photo-1520409364224-63400afe26e5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80",
-    },
-    {
-      name: "Another Guy",
-      imgUrl:
-        "https://images.unsplash.com/photo-1519085360753-af0119f7cbe7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
-    },
-    {
-      name: "Random Girl",
-      imgUrl:
-        "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80",
-    },
-    {
-      name: "Another Girl",
-      imgUrl:
-        "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80",
-    },
-  ]);
+  const [currentIndex, setCurrentIndex] = useState(db.length - 1);
+  const [lastDirection, setLastDirection] = useState<string>();
+  // used for outOfFrame closure
+  const currentIndexRef = useRef(currentIndex);
 
-  const swiped = (direction: string, name: Person["name"]) => {
-    console.log("Receiving " + name);
+  const childRefs = useMemo<RefObject<ChildRef>[]>(
+    () =>
+      Array(db.length)
+        .fill(0)
+        .map((i) => React.createRef()),
+    []
+  );
+
+  const updateCurrentIndex = (val: number) => {
+    setCurrentIndex(val);
+    currentIndexRef.current = val;
   };
 
-  const outOfFrame = (name: Person["name"]) => {
-    console.log(name + " left the screen");
+  const canGoBack = currentIndex < db.length - 1;
+
+  const canSwipe = currentIndex >= 0;
+
+  const swiped = (
+    direction: Direction,
+    nameToDelete: string,
+    index: number
+  ) => {
+    setLastDirection(direction);
+    updateCurrentIndex(index - 1);
+  };
+
+  const outOfFrame = (name: string, idx: number) => {
+    console.log(`${name} (${idx}) left the screen!`, currentIndexRef.current);
+    currentIndexRef.current >= idx && childRefs[idx].current?.restoreCard();
+  };
+
+  const swipe = async (dir: Direction) => {
+    if (canSwipe && currentIndex < db.length) {
+      await childRefs[currentIndex].current?.swipe(dir);
+    }
+  };
+
+  const goBack = async () => {
+    if (!canGoBack) return;
+    const newIndex = currentIndex + 1;
+    updateCurrentIndex(newIndex);
+    await childRefs[newIndex].current?.restoreCard();
   };
 
   return (
     <DatingCardsContainer>
-      {people.map((person) => (
-        <DatingCard
-          key={person.name}
-          onSwipe={(dir) => swiped(dir, person.name)}
-          onCardLeftScreen={() => outOfFrame(person.name)}
-          preventSwipe={["up", "down"]}
-          className="datingCard"
+      <h1>React Tinder Card</h1>
+      <div className="cardContainer">
+        {db.map((character, index) => (
+          <TinderCard
+            ref={childRefs[index]}
+            className="datingCard"
+            key={character.name}
+            onSwipe={(dir) => swiped(dir, character.name, index)}
+            onCardLeftScreen={() => outOfFrame(character.name, index)}
+          >
+            <div
+              style={{ backgroundImage: "url(" + character.url + ")" }}
+              className="card"
+            >
+              <h3>{character.name}</h3>
+            </div>
+          </TinderCard>
+        ))}
+      </div>
+      <div className="buttons">
+        <button
+          // @ts-ignore
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("left")}
         >
-          <div style={{ backgroundImage: `url(${person.imgUrl})` }}>
-            <h3>{person.name}</h3>
-          </div>
-        </DatingCard>
-      ))}
+          Swipe left!
+        </button>
+        <button
+          // @ts-ignore
+          style={{ backgroundColor: !canGoBack && "#c3c4d3" }}
+          onClick={() => goBack()}
+        >
+          Undo swipe!
+        </button>
+        <button
+          // @ts-ignore
+          style={{ backgroundColor: !canSwipe && "#c3c4d3" }}
+          onClick={() => swipe("right")}
+        >
+          Swipe right!
+        </button>
+      </div>
+      {lastDirection ? (
+        <h2 key={lastDirection} className="infoText">
+          You swiped {lastDirection}
+        </h2>
+      ) : (
+        <h2 className="infoText">
+          Swipe a card or press a button to get Restore Card button visible!
+        </h2>
+      )}
     </DatingCardsContainer>
   );
 };
--- dating-app-mern/frontend/src/components/DatingCards/styles.ts
+++ dating-app-mern/frontend/src/components/DatingCards/styles.ts
@@ -4,6 +4,7 @@ import macro from "styled-components/macro";
 export const DatingCardsContainer = styled.section`
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 `;
