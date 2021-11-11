8e6c695725be1aedbb4deb09d74f8736d9e9e2a7
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-11 14:11:53 +0300

    backend initial setup

---
+++ dating-app-mern/backend/src/server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import helmet from "helmet";
+import { ApolloServer } from "apollo-server-express";
+// import db from "./db";
+// import typeDefs from "./schema";
+// import resolvers from "./resolvers";
+
+const port = process.env.PORT || 8001;
+const host = process.env.DB_HOST;
+const root = process.env.ROOT;
+
+// db.connect(host);
+
+const app = express();
+app.use(cors());
+app.use(helmet());
+
+const server = new ApolloServer({
+  // typeDefs, resolvers
+});
+
+server.applyMiddleware({ app, path: root });
+
+app.listen({ port }, () => {
+  console.log(`listening on port ${port}`);
+});
