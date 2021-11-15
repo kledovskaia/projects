01b618e2a0998a1f75d8ad1488253e3c1126522c
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-15 07:56:04 +0300

    simple routing

--- simple-node-app/helloworld.js
+++ simple-node-app/helloworld.js
@@ -2,8 +2,21 @@ const http = require("http");
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Hello World!");
+  const path = req.url.replace(/\/?(?:\?.*)?$/, "").toLowerCase();
+
+  switch (path) {
+    case "":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("Home Page");
+      break;
+    case "/about":
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end("About Page");
+      break;
+    default:
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Found");
+  }
 });
 
 server.listen(port, () => {
