8bb8e0e6969de88471d98dae1b00ad00393da69b
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-17 15:31:29 +0300

    saving todos to localStorage

--- todo/src/redux/todos.ts
+++ todo/src/redux/todos.ts
@@ -2,7 +2,7 @@ import { v4 } from "uuid"
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
 
 const initialState = {
-  todos: [] as TTodo[],
+  todos: (JSON.parse(localStorage.getItem("todos")!) || []) as TTodo[],
   todoOnEdit: null as null | TTodo | { [key in string]: string },
 }
 
@@ -12,11 +12,13 @@ const todosSlice = createSlice({
   reducers: {
     add: (state, action: PayloadAction<TTodo["content"]>) => {
       state.todos.push(createTodoObject(action.payload))
+      localStorage.setItem("todos", JSON.stringify(state.todos))
     },
     remove: (state, action: PayloadAction<TTodo["id"]>) => {
       const index = state.todos.findIndex((todo) => todo.id === action.payload)
       if (index === -1) return
       state.todos.splice(index, 1)
+      localStorage.setItem("todos", JSON.stringify(state.todos))
     },
     toggleDone: (state, action: PayloadAction<TTodo["id"]>) => {
       const index = state.todos.findIndex((todo) => todo.id === action.payload)
@@ -25,6 +27,7 @@ const todosSlice = createSlice({
         ...state.todos[index],
         done: !state.todos[index].done,
       })
+      localStorage.setItem("todos", JSON.stringify(state.todos))
     },
     changeContent: (
       state,
@@ -38,6 +41,7 @@ const todosSlice = createSlice({
       )
       if (index === -1) return
       state.todos[index].content = action.payload.newContent
+      localStorage.setItem("todos", JSON.stringify(state.todos))
     },
     setTodoOnEdit: (
       state,
@@ -56,6 +60,7 @@ const todosSlice = createSlice({
         state.todoOnEdit = {}
         return
       }
+      localStorage.setItem("todos", JSON.stringify(state.todos))
     },
   },
 })
