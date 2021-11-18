401db753571afd0008cfd47e10ee7f693875e049
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-18 10:45:07 +0300

    fixed authorization

--- dating-app-mern/frontend/src/index.tsx
+++ dating-app-mern/frontend/src/index.tsx
@@ -1,41 +1,42 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import "./index.css";
-import { App } from "./App";
+import React from "react"
+import ReactDOM from "react-dom"
+import { BrowserRouter as Router } from "react-router-dom"
+import "./index.css"
+import { App } from "./App"
 import {
   ApolloClient,
   ApolloProvider,
   createHttpLink,
   InMemoryCache,
-} from "@apollo/client";
-import { setContext } from "apollo-link-context";
-import dotenv from "dotenv";
-import { AuthContextProvider } from "./context/Auth";
-dotenv.config();
+} from "@apollo/client"
+import dotenv from "dotenv"
+import { AuthContextProvider } from "./context/Auth"
+import { setContext } from "apollo-link-context"
+dotenv.config()
 
-const uri = process.env.API_URI || "http://localhost:3000/";
+const uri = process.env.API_URI || "http://localhost:3000/"
 
 const httpLink = createHttpLink({
   uri,
   credentials: "same-origin",
-});
+})
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("dating-app-token");
-
+  const token = localStorage.getItem("token")
   return {
-    ...headers,
-    Authorization: token || "",
-  };
-});
+    headers: {
+      ...headers,
+      Authorization: token || "",
+    },
+  }
+})
 
 const client = new ApolloClient({
   // @ts-ignore
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
   connectToDevTools: true,
-});
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -48,4 +49,4 @@ ReactDOM.render(
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
