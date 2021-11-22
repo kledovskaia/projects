14f36c45b19b196841bda6329d8e8ba86e7ec1ff
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-22 12:44:47 +0300

    rm

--- notedly/client/src/hooks/useDocumentTitle.ts
+++
@@ -1,3 +0,0 @@
-export const useDocumentTitle = (title: string) => {
-  document.title = `${title} | Notedly`
-}
--- notedly/client/src/pages/SignIn/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import macro from "styled-components/macro"
-
-export const Container = styled.div`
-  padding: 2rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`
