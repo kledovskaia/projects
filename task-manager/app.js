0e751680514ec5f2b3b32d472fc92289fe861b44
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-25 06:17:30 +0300

    basic api structure

--- task-manager/app.js
+++ task-manager/app.js
@@ -1,1 +1,15 @@
-console.log('Task Manager App')
+import tasks from "./routes/tasks.js"
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(express.json())
+
+app.use("/api/v1/tasks", tasks)
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`)
+})
---
+++ task-manager/controllers/tasks.js
@@ -0,0 +1,15 @@
+export const getAll = (req, res) => {
+  res.send("getAll")
+}
+export const getOne = (req, res) => {
+  res.send("getOne")
+}
+export const createOne = (req, res) => {
+  res.send("createOne")
+}
+export const updateOne = (req, res) => {
+  res.send("updateOne")
+}
+export const deleteOne = (req, res) => {
+  res.send("deleteOne")
+}
