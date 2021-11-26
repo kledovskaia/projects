f9d043cfe212f7160cd4c87e7698afd047a9cbe6
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-26 11:05:07 +0300

    unexisting route

--- task-manager/app.js
+++ task-manager/app.js
@@ -3,12 +3,14 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
 import { connectDB } from "./db/connect.js"
+import { notFound } from "./middleware/not-found.js"
 
 const app = express()
 const PORT = process.env.PORT
 app.use(express.static("./public"))
 app.use(express.json())
 app.use("/api/v1/tasks", tasks)
+app.use(notFound)
 
 connectDB(process.env.MONGO_URL).then(() => {
   app.listen(PORT, () => {
