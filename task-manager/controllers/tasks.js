971f631af0c55633117fe829e14635c493869f8d
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-26 08:08:15 +0300

    delete and update

--- task-manager/controllers/tasks.js
+++ task-manager/controllers/tasks.js
@@ -21,15 +21,37 @@ export const createOne = async (req, res) => {
   try {
     const { content } = req.body
     const task = await Task.create({ content })
-    if (!task) throw new Error("Error Creating a Task")
+    if (!task) throw new Error()
     res.status(200).json({ task })
   } catch (error) {
-    res.status(500).json({ message: error })
+    console.log(error)
+    res.status(500).json({ message: "Error Creating a Task" })
   }
 }
-export const updateOne = (req, res) => {
-  res.send("updateOne")
+export const updateOne = async (req, res) => {
+  try {
+    const task = await Task.findOneAndUpdate({ _id: req.params.id }, req.body, {
+      new: true,
+      runValidators: true,
+    })
+    if (!task) throw new Error()
+    res.status(200).json({ task })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: `Error updating the task with id ${req.params.id}` })
+  }
 }
-export const deleteOne = (req, res) => {
-  res.send("deleteOne")
+export const deleteOne = async (req, res) => {
+  try {
+    const task = await Task.findOneAndDelete({ _id: req.params.id })
+    if (!task) throw new Error()
+    res.status(200).json({ task })
+  } catch (error) {
+    console.log(error)
+    res
+      .status(500)
+      .json({ message: `Error deleting the task with id ${req.params.id}` })
+  }
 }
