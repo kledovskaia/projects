8f66a9542c927a76d4dd253bca44a3f1829d17a8
maddison-marshall <MaddisonMarshall2001@gmail.com>
2021-11-27 04:11:50 +0300

    init

--- notedly/api/src/resolvers/note.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  author: async (note, _, { models }) =>
-    await models.User.findById(note.author),
-  favoritedBy: async (note, _, { models }) =>
-    await models.User.find({ _id: { $in: note.favoritedBy } })
-};
