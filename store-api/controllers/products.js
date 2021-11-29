eda7f9873ce2a0f9c5ca3bf8d5780c155f95238f
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-11-29 13:25:50 +0300

    numberic filters

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -2,8 +2,9 @@ import { Product } from '../models/product.js'
 
 export const get = async (req, res) => {
   const {
-    page,
-    perPage,
+    numericFilters,
+    page = 1,
+    perPage = 10,
     sort,
     fields,
     name,
@@ -12,6 +13,7 @@ export const get = async (req, res) => {
     featured,
     company,
   } = req.query
+
   const queryObject = {
     ...(name ? { name: { $regex: name.trim(), $options: 'i' } } : {}),
     ...(price ? { price: +price } : {}),
@@ -19,12 +21,34 @@ export const get = async (req, res) => {
     ...(featured ? { featured: featured === 'true' ? true : false } : {}),
     ...(company ? { company } : {}),
   }
+
+  if (numericFilters) {
+    const operators = {
+      '>': '$gt',
+      '>=': '$gte',
+      '=': '$eq',
+      '<': '$lt',
+      '<=': '$lte',
+    }
+    const converted = numericFilters.replace(
+      /\b(>|<|=|>=|>)\b/g,
+      (match) => `-${operators[match]}-`
+    )
+    converted.split(',').forEach((string) => {
+      const splited = string.split('-')
+      queryObject[splited[0]] = {
+        [splited[1]]: +splited[2],
+      }
+    })
+  }
+
   const result = Product.find(queryObject)
+    .skip((page - 1) * perPage)
     .limit(+perPage)
-    .skip(+page)
+
   if (sort) result.sort(sort.split(',').join(' '))
   else result.sort('createdAt')
   if (fields) result.select(fields.split(',').join(' '))
   const products = await result
-  res.status(200).json({ products })
+  res.status(200).json({ products, count: products.length })
 }
