8350551e0cd64ccda0cd94cda08cc34fa4c530e6
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-11-29 13:41:42 +0300

    rm

--- dating-app-mern/frontend/src/hooks/useAppQuery.ts
+++
@@ -1,7 +0,0 @@
-import { OperationVariables, useQuery } from "@apollo/client";
-import * as query from "../graphql/query";
-
-type TQuery = keyof typeof query;
-
-export const useAppQuery = <T>(type: TQuery, options?: OperationVariables) =>
-  useQuery<T>(query[type], options);
