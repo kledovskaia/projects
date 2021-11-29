e06f12c7f5a1ccadb19582036227f5876803625f
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-11-29 13:49:36 +0300

    rm

--- notedly/client/src/pages/EditNote/index.ts
+++
@@ -1,1 +0,0 @@
-export { EditNote } from "./EditNote"
--- todo/src/components/App/styles.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components"
-import macro from "styled-components/macro"
-
-export const AppContainer = styled.section`
-  padding: 4rem 0;
-  margin: 0 auto;
-  max-width: 50rem;
-  width: 90vw;
-`
