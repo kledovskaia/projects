51fbd625c151b821d198122fcdeeadc24709e1bd
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-11-29 05:00:33 +0300

    product model

--- store-api/app.js
+++ store-api/app.js
@@ -5,15 +5,16 @@ import { notFound } from './middleware/not-found.js'
 import { errorHandler } from './middleware/error-handler.js'
 import { products } from './routes/products.js'
 import { connectDB } from './db/connect.js'
+import 'express-async-errors'
 
 const app = express()
 const port = process.env.PORT || 3001
 
-app.get('/', (req, res) => res.send("<h1>Hello World!</h1>"))
+app.get('/', (req, res) => res.send('<h1>Hello World!</h1>'))
 
+app.use('/api/v1/products', products)
 app.use(notFound)
 app.use(errorHandler)
-app.use('/api/v1/products', products)
 
 connectDB(process.env.MONGO_URI)
   .then(() => {
--- store-api/models/product.js
+++ store-api/models/product.js
@@ -1,7 +1,33 @@
 import mongoose from 'mongoose'
 
 const productSchema = new mongoose.Schema({
-    name: String   
+  name: {
+    type: String,
+    require: [true, 'name must be provided'],
+  },
+  price: {
+    type: Number,
+    require: [true, 'price must be provided'],
+  },
+  rating: {
+    type: Number,
+    default: 0,
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['ikea', 'liddy', 'caressa', 'marcos'],
+      message: '{VALUE} is not provided',
+    },
+  },
 })
 
-export const Product = mongoose.model("Product", productSchema)
+export const Product = mongoose.model('Product', productSchema)
