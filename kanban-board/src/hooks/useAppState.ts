be857e37936a9cae222de3d40f38975eda1877c3
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-11-29 13:58:01 +0300

    rm

--- kanban-board/src/hooks/useAppState.ts
+++
@@ -1,6 +0,0 @@
-import { useContext } from 'react'
-import { AppStateContext } from '../state/AppStateProvider'
-
-export const useAppState = () => {
-  return useContext(AppStateContext)
-}
--- store-api/routes/products.js
+++
@@ -1,7 +0,0 @@
-import express from 'express'
-import * as products from '../controllers/products.js'
-
-const router = express.Router()
-
-router.route('/').get(products.get)
-export { router as products }
