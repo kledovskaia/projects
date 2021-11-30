fc30be5f3d92a074d104421ff4e8f826de33b525
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-11-30 05:59:02 +0300

    rm

--- notedly/client/src/hooks/useAppQuery.ts
+++
@@ -1,8 +0,0 @@
-import { OperationVariables, useQuery } from "@apollo/client"
-import * as queries from "../graphql/queries"
-
-type TQuery = keyof typeof queries
-
-export const useAppQuery = <T>(type: TQuery, options?: OperationVariables) => {
-  return useQuery<T>(queries[type], options)
-}
