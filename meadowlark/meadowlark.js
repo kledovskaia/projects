f02135b693cd0910e6cfab95d0a9ef7910a99877
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2021-12-22 13:11:12 +0300

    handlebars

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,15 +1,36 @@
+const path = require('path')
 const express = require('express')
+const { engine } = require('express-handlebars')
 require('dotenv').config()
 
 const app = express()
 const PORT = process.env.PORT || 3001
 
+app.engine(
+  'handlebars',
+  engine({
+    defaultLayout: 'main',
+  })
+)
+app.set('view engine', 'handlebars')
+app.set('views', path.resolve(__dirname, './views'))
+
+app.get('/', (req, res) => {
+  res.render('home')
+})
+
+app.get('/about', (req, res) => {
+  res.render('about')
+})
+
 app.use((req, res) => {
-  res.type('text/plain').status(404).send('404 - Not Found')
+  res.status(404)
+  res.render('404')
 })
 app.use((err, req, res, next) => {
   console.log(err.message)
-  res.type('text/plain').status(500).send('500 - Server Error')
+  res.status(500)
+  res.render('500')
 })
 
 app.listen(PORT, () => {
