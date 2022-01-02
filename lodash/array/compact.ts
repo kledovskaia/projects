481c6fe33bb70b5766588a87537a609cb379aa5f
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2022-01-02 20:25:18 +0300

    compact reduce

--- lodash/array/compact.ts
+++ lodash/array/compact.ts
@@ -7,6 +7,7 @@ const compactFor: TCompact = (array) => {
   return result
 }
 const compactFilter: TCompact = (array) => array.filter(item => item)
+const compactReduce: TCompact = <T>(array: T[]): T[] => array.reduce((res, item) => (item ? [...res, item] : res), [] as T[])
 
-console.log(compactFilter([0, 1, false, 2, '', 3]))
+console.log(compactReduce([0, 1, false, 2, '', 3]))
 // => [1, 2, 3]
