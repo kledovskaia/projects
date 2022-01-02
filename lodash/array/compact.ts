eba3e72d00c3a319bc3f7201dc07184e8dde9cfc
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2022-01-02 20:20:23 +0300

    compact filter

--- lodash/array/compact.ts
+++ lodash/array/compact.ts
@@ -1,11 +1,12 @@
 type TCompact = <T>(array: T[]) => T[];
-const compact: TCompact = (array) => {
+const compactFor: TCompact = (array) => {
   const result = [];
   for (const item of array) {
     if (item) result.push(item)
   }
   return result
 }
+const compactFilter: TCompact = (array) => array.filter(item => item)
 
-console.log(compact([0, 1, false, 2, '', 3]))
+console.log(compactFilter([0, 1, false, 2, '', 3]))
 // => [1, 2, 3]
