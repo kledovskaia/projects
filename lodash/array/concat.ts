2c23fb4497c1a0167012076479f9458664710cc1
maddison-matthews <MaddisonMatthewsWebDev@gmail.com>
2022-01-02 20:58:36 +0300

    concat for

---
+++ lodash/array/concat.ts
@@ -0,0 +1,20 @@
+type TConcat = <T, K>(array: T[], ...values: K[]) => (T | K)[];
+const concatFor: TConcat = (array, ...values) => {
+  const result = [];
+  result.push(...array);
+  if (!values) return result;
+  for (const value of values) {
+    if (Array.isArray(value)) result.push(...value);
+    else result.push(value)
+  }
+  return result
+}
+
+var array = [1];
+var other = concatFor(array, [2], [3], [[4]]);
+ 
+console.log(other);
+// => [1, 2, 3, [4]]
+ 
+console.log(array);
+// => [1]
