c109d05c9d71a0b456a067f0269c45b52014dde2
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-02-08 12:21:23 +0300

    integration test

--- meadowlark/meadowlark.js
+++ meadowlark/meadowlark.js
@@ -1,28 +1,32 @@
-require('dotenv').config()
-const path = require('path')
-const express = require('express')
-const { engine } = require('express-handlebars')
-const handlers = require('./lib/handlers')
+require('dotenv').config();
+const path = require('path');
+const express = require('express');
+const { engine } = require('express-handlebars');
+const handlers = require('./lib/handlers');
 
-const app = express()
-const PORT = process.env.PORT || 3001
+const app = express();
+const PORT = process.env.PORT || 3001;
 
 app.engine(
   'handlebars',
   engine({
     defaultLayout: 'main',
   })
-)
-app.set('view engine', 'handlebars')
+);
+app.set('view engine', 'handlebars');
 
-app.use(express.static(path.resolve(__dirname + '/public')))
+app.use(express.static(path.resolve(__dirname + '/public')));
 
-app.get('/', handlers.home)
-app.get('/about', handlers.about)
+app.get('/', handlers.home);
+app.get('/about', handlers.about);
 
-app.use(handlers.notFound)
-app.use(handlers.serverError)
+app.use(handlers.notFound);
+app.use(handlers.serverError);
 
-app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`)
-})
+if (require.main === module) {
+  app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+  });
+} else {
+  module.exports = app;
+}
