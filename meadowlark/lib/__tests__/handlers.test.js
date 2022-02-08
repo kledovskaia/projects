2e89eff4d9b544dc28578485086c738417448f2e
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-02-08 05:06:04 +0300

    tests added: render pages

--- meadowlark/lib/__tests__/handlers.test.js
+++ meadowlark/lib/__tests__/handlers.test.js
@@ -1,8 +1,31 @@
-const handlers = require('../handlers')
+const handlers = require('../handlers');
 
 test('home page renders', () => {
-  const req = {}
-  const res = { render: jest.fn() }
-  handlers.home(req, res)
-  expect(res.render.mock.calls[0][0]).toBe('home')
-})
+  const req = {};
+  const res = { render: jest.fn() };
+  handlers.home(req, res);
+  expect(res.render.mock.calls[0][0]).toBe('home');
+});
+
+test('about page renders', () => {
+  const req = {};
+  const res = { render: jest.fn() };
+  handlers.about(req, res);
+  expect(res.render.mock.calls[0][0]).toBe('about');
+});
+
+test('404 page renders', () => {
+  const req = {};
+  const res = { render: jest.fn(), status: jest.fn() };
+  handlers.notFound(req, res);
+  expect(res.render.mock.calls[0][0]).toBe('404');
+});
+
+test('500 page renders', () => {
+  const err = new Error();
+  const req = {};
+  const res = { render: jest.fn(), status: jest.fn() };
+  const next = jest.fn();
+  handlers.serverError(err, req, res, next);
+  expect(res.render.mock.calls[0][0]).toBe('500');
+});
