0193ed0bd7f232fcc96e8eb2e4eb63a5d790cb29
fuxleep <MaddisonMatthewsWebDev@gmail.com>
2022-02-08 05:11:09 +0300

    tests: update

--- meadowlark/lib/__tests__/handlers.test.js
+++ meadowlark/lib/__tests__/handlers.test.js
@@ -4,6 +4,7 @@ test('home page renders', () => {
   const req = {};
   const res = { render: jest.fn() };
   handlers.home(req, res);
+  expect(res.render.mock.calls.length).toBe(1);
   expect(res.render.mock.calls[0][0]).toBe('home');
 });
 
@@ -11,13 +12,20 @@ test('about page renders', () => {
   const req = {};
   const res = { render: jest.fn() };
   handlers.about(req, res);
+  expect(res.render.mock.calls.length).toBe(1);
   expect(res.render.mock.calls[0][0]).toBe('about');
+  expect(res.render.mock.calls[0][1]).toEqual(
+    expect.objectContaining({
+      fortune: expect.stringMatching(/\W+/),
+    })
+  );
 });
 
 test('404 page renders', () => {
   const req = {};
   const res = { render: jest.fn(), status: jest.fn() };
   handlers.notFound(req, res);
+  expect(res.render.mock.calls.length).toBe(1);
   expect(res.render.mock.calls[0][0]).toBe('404');
 });
 
@@ -27,5 +35,6 @@ test('500 page renders', () => {
   const res = { render: jest.fn(), status: jest.fn() };
   const next = jest.fn();
   handlers.serverError(err, req, res, next);
+  expect(res.render.mock.calls.length).toBe(1);
   expect(res.render.mock.calls[0][0]).toBe('500');
 });
